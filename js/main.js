f2b9ada74caae1519bf7b8586c49bb143a77c703
Vagnermrocha <vagnermendesrocha@gmail.com>
2023-06-13 11:38:00 -0300

    arquivos iniciais

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criarElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+}
